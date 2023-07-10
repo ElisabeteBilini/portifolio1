d827f061e51693611d0f4394468aa6793d1ad2a4
ELISABETE L BILINI <elisabetebilini@gmail.com>
2023-07-10 23:46:33 +0000

    fix input username and Fix Final message

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,20 @@
      })
  
      if(collectName !== null) collectName.textContent = localStorage.getItem('userName');
+
+
+//aceitar somente letras input usuario
+     function lettersOnly(evt) {
+    evt = (evt) ? evt : event;
+    var charCode = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode :
+        ((evt.which) ? evt.which : 0));
+    if (charCode > 31 && (charCode < 65 || charCode > 90) &&
+        (charCode < 97 || charCode > 122)) {
+        alert("Enter letters only.");
+        return false;
+    }
+    return true;
+}
  
  
  //-----------------------------------------GAME STRUCTURE---------------------------------------------------
